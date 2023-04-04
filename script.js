92bac2070e7e6fe3f9fd3330f0451714513b72f4
Sheharyar Ahmad <phantomwitch35@gmail.com>
2023-04-04 17:46:28 +0500

    Set up index and contact page

---
+++ script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const dropdownLink = document.querySelector(".menu-trigger");
+const dropdownMenu = document.querySelector(".dropdown");
+dropdownLink.addEventListener("mouseover", () => {
+  dropdownLink.classList.add("show-dropdown");
+  dropdownMenu.classList.add("show");
+});
+
+dropdownLink.addEventListener("mouseout", () => {
+  dropdownLink.classList.remove("show-dropdown");
+  dropdownMenu.classList.remove("show");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const selectElement = document.querySelector(".form-select");
+  const options = document.querySelectorAll("option");
+
+  options.forEach((option) => option.classList.add("custom-option"));
+
+  selectElement.addEventListener("focus", () => {
+    options.forEach((option) => {
+      option.style.backgroundColor = "#fff";
+    });
+  });
+
+  // selectElement.addEventListener("change", () => {
+  //   options.forEach((option) => {
+  //     option.style.backgroundColor = option.selected ? "#ff" : "#f77700";
+  //   });
+  // });
+});
