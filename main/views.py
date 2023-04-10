4449e299ef97d57754f8beb5a3996be424a48e8c
Sheharyar Ahmad <phantomwitch35@gmail.com>
2023-04-10 16:30:13 +0500

    make index page dynamic

--- main/views.py
+++ main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, View
+from .forms import ContactForm
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = "main/index.html"
+
+
+class ContactPageView(View):
+    def get(self, request, *args, **kwargs):
+        form = ContactForm()
+        context = {"form": form}
+        return render(request, "main/contact.html", context)
