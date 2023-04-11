8b56ec7a0e1bde02969cdb696d7899ba1ecfd4c3
Sheharyar Ahmad <phantomwitch35@gmail.com>
2023-04-11 14:45:05 +0500

    make all the services pages dynamic

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,46 @@
 from django.urls import path
-from .views import HomePageView
+from .views import (
+    HomePageView,
+    ServicePage1View,
+    ServicePage2View,
+    ServicePage3View,
+    ServicePage4View,
+    ServicePage5View,
+    ServicePage6View,
+)
+
+app_name = "main"
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path(
+        "services/implementation_consulting/",
+        ServicePage1View.as_view(),
+        name="service1",
+    ),
+    path(
+        "services/sap_services_consulting/",
+        ServicePage2View.as_view(),
+        name="service2",
+    ),
+    path(
+        "services/process_consulting/",
+        ServicePage3View.as_view(),
+        name="service3",
+    ),
+    path(
+        "services/project_management/",
+        ServicePage4View.as_view(),
+        name="service4",
+    ),
+    path(
+        "services/development/",
+        ServicePage5View.as_view(),
+        name="service5",
+    ),
+    path(
+        "services/support/",
+        ServicePage6View.as_view(),
+        name="service6",
+    ),
 ]
--- main/views.py
+++ main/views.py
@@ -1,14 +1,29 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, View
-from .forms import ContactForm
+from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
     template_name = "main/index.html"
 
 
-class ContactPageView(View):
-    def get(self, request, *args, **kwargs):
-        form = ContactForm()
-        context = {"form": form}
-        return render(request, "main/contact.html", context)
+class ServicePage1View(TemplateView):
+    template_name = "main/services1.html"
+
+
+class ServicePage2View(TemplateView):
+    template_name = "main/services2.html"
+
+
+class ServicePage3View(TemplateView):
+    template_name = "main/services3.html"
+
+
+class ServicePage4View(TemplateView):
+    template_name = "main/services4.html"
+
+
+class ServicePage5View(TemplateView):
+    template_name = "main/services5.html"
+
+
+class ServicePage6View(TemplateView):
+    template_name = "main/services6.html"
